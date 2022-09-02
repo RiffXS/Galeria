bfb88ffd47f487bbac6480097c282e5c777d01dc
Rafael <rafaelbleaob@gmail.com>
2022-09-02 09:57:15 -0300

    Progresso do Projeto

--- app/src/main/java/barros/rafael/galeria/MainAdapter.java
+++ app/src/main/java/barros/rafael/galeria/MainAdapter.java
@@ -1,5 +1,13 @@
 package barros.rafael.galeria;
 
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -12,5 +20,37 @@ public class MainAdapter extends RecyclerView.Adapter {
     public MainAdapter (MainActivity mainActivity, List<String> photos) {
         this.mainActivity = mainActivity;
         this.photos = photos;
+    }
+
+    @NonNull
+    @Override
+    // Constrói o item, retorna a interface gráfica e envia para o guardador de view
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Cria um elemento de interface referente a um objeto
+        LayoutInflater inflater = LayoutInflater.from(mainActivity);
+        // Contrói o item com o layout (.xml) definido
+        View v = inflater.inflate(R.layout.list_item, parent, false);
+        // Retorna o ViewHolder guardando o item criado
+        return new ViewHolder(v);
+    }
+
+    @Override
+    // Retorna a quantidade de itens atual
+    public int getItemCount() { return photos.size(); }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+        ImageView imPhoto = holder.itemView.findViewById(R.id.imItem);
+        int w = (int) mainActivity.getResources().getDimension(R.dimen.itemWidth);
+        int h = (int) mainActivity.getResources().getDimension(R.dimen.itemHeight);
+        Bitmap bitmap = Utils.getBitmap(photos.get(position), w, h);
+
+        imPhoto.setImageBitmap(bitmap);
+        imPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                mainActivity.startPhotoActivity(photos.get(position));
+            }
+        });
     }
 }
